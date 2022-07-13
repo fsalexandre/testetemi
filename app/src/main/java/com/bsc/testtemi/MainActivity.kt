71069a652c3ca8af442cd3fd7b9843ec8872d225
Alexandre Fernandes da Silva <fullface@alexandrefs.com>
2022-07-13 03:01:32 -0300

    Refactoring:
    Added the draft of UI with Jetpack Compose getting data from external API.

--- app/src/main/java/com/bsc/testtemi/MainActivity.kt
+++ app/src/main/java/com/bsc/testtemi/MainActivity.kt
@@ -1,34 +1,99 @@
 package com.bsc.testtemi
 
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.findNavController
+import com.bsc.testtemi.data.model.ListSetor
+import com.bsc.testtemi.ui.setor.SetorViewModel
 import kotlinx.android.synthetic.main.main_activity.*
+import org.koin.androidx.viewmodel.ext.android.getViewModel
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
-        setButtonAction()
+
+        setContent {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    SetorListScreen()
+                }
+
+        }
+       //setContentView(R.layout.main_activity)
+        //setButtonAction()
     }
 
-    fun setButtonAction(){
-        btnQuery.setOnClickListener {
-            val txtQuery = txtQuery.text.toString()
-            val bundle = Bundle()
-            bundle.putString("strQuery", txtQuery)
-            findNavController(R.id.container).navigate(R.id.ProdutosFragment, bundle)
+
+    @Composable
+    fun SetorListScreen(){
+        //val viewModel: SetorViewModel by viewModels()
+        val viewModel: SetorViewModel = getViewModel()
+        //val viewModel = viewModel(modelClass = SetorViewModel::class.java)
+        viewModel.requestSetor()
+        val state by viewModel.state.collectAsState()
+        LazyColumn {
+            if (state.isEmpty()) {
+                item {
+                    CircularProgressIndicator(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .wrapContentSize(align = Alignment.Center)
+                    )
+                }
+
+            }
+
+            items(state) { character: ListSetor ->
+                Text(text = character.descSetor.toString(),
+                    fontSize = 25.sp
+                )
+            }
+
+
         }
+
     }
 
-    override fun onBackPressed() {
-        val fragments = supportFragmentManager.backStackEntryCount
-        if (fragments == 1) {
-            finish()
-            return
-        }
-        super.onBackPressed()
-    }
+
+//    fun setButtonAction(){
+//        btnQuery.setOnClickListener {
+//            val txtQuery = txtQuery.text.toString()
+//            val bundle = Bundle()
+//            bundle.putString("strQuery", txtQuery)
+//            findNavController(R.id.container).navigate(R.id.ProdutosFragment, bundle)
+//        }
+//    }
+//
+//    override fun onBackPressed() {
+//        val fragments = supportFragmentManager.backStackEntryCount
+//        if (fragments == 1) {
+//            finish()
+//            return
+//        }
+//        super.onBackPressed()
+//    }
 }
--- app/src/main/java/com/bsc/testtemi/ui/setor/SetorViewModel.kt
+++ app/src/main/java/com/bsc/testtemi/ui/setor/SetorViewModel.kt
@@ -1,24 +1,36 @@
 package com.bsc.testtemi.ui.setor
 
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.bsc.testtemi.data.api.Repository
 import com.bsc.testtemi.data.model.ListSetor
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 
-class SetorViewModel(private val repository: Repository) : ViewModel() {
+class SetorViewModel (private val repository: Repository) : ViewModel() {
 
     val setorList = MutableLiveData<List<ListSetor>>()
     val errorMessage = MutableLiveData<String>()
+
+    private val _setorListCompose = mutableListOf<List<ListSetor>>()
+    val setorListCompose: MutableList<List<ListSetor>> = _setorListCompose
+
+    private val _state = MutableStateFlow(emptyList<ListSetor>())
+    val state: StateFlow<List<ListSetor>>
+        get() = _state
 
     fun requestSetor() {
         val response = repository.requestSetor()
         response.enqueue(object : Callback<List<ListSetor>> {
             override fun onResponse(call: Call<List<ListSetor>>, response: Response<List<ListSetor>>) {
                 setorList.postValue(response.body())
+                _state.value = response.body()!!
             }
             override fun onFailure(call: Call<List<ListSetor>>, t: Throwable) {
                 errorMessage.postValue(t.message)
